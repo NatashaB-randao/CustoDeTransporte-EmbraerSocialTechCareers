c85865cb48f980d7e84299917c51f0c87aa11452
NatashaB-randao <natashabrandao57@hotmail.com>
2024-01-07 14:31:24 -0300

    Entity and Relationship Diagram Example

--- Entity and Relationship Diagram Example.sql
+++ Entity and Relationship Diagram Example.sql
@@ -41,3 +41,13 @@ SET foreign_key_checks = 0;
 ALTER TABLE BD_aumentado_comercial
 ADD CONSTRAINT fk_destino_pais
 FOREIGN KEY (Destino) REFERENCES BD_País_e_CiaAerea(País);
+
+-- Esse código SQL está adicionando uma restrição de chave estrangeira (FOREIGN KEY) à tabela BD_aumentado_comercial:
+
+-- ALTER TABLE BD_aumentado_comercial: Indica que deseja fazer uma alteração na estrutura da tabela chamada BD_aumentado_comercial.
+-- ADD CONSTRAINT fk_destino_pais: Está adicionando uma nova restrição (constraint) à tabela, e essa restrição é chamada de fk_destino_pais. O nome fk_destino_pais é uma convenção comum que sugere que essa restrição é uma chave estrangeira relacionada ao destino e país.
+-- FOREIGN KEY (Destino): Define a coluna Destino na tabela BD_aumentado_comercial como a chave estrangeira.
+-- REFERENCES BD_País_e_CiaAerea(País): Indica que a chave estrangeira Destino na tabela BD_aumentado_comercial faz referência à coluna País na tabela BD_País_e_CiaAerea. Isso estabelece um vínculo entre as duas tabelas, onde os valores na coluna Destino devem corresponder aos valores na coluna País da tabela referenciada.
+
+-- Em resumo, essa instrução está criando uma relação entre as tabelas BD_aumentado_comercial e BD_País_e_CiaAerea, 
+-- onde a coluna Destino em BD_aumentado_comercial funciona como uma chave estrangeira que faz referência à coluna País em BD_País_e_CiaAerea. 
